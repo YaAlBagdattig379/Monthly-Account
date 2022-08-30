73a0db1019c74169a4dc3b5d6e871604b5700296
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-08-30 16:24:34 +0530

    added second

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
-function valueIput(){
+    
+document.getElementById("calculate-button").addEventListener("click",function(){
+    // input income   
     const incomeInput = document.getElementById("income-input-box");
     const incomeValue = incomeInput.value ;
     const incomeText = parseInt(incomeValue);
-    console.log(incomeText);
-}
-document.getElementById("calculate-button").addEventListener("click",function(){
-    valueIput()
-    // const incomeInput = document.getElementById("income-input-box");
-    // const incomeValue = incomeInput.value ;
-    // const incomeText = parseInt(incomeValue);// .value;
-    // console.log(incomeText);
-    // console.log("hello ! hi javaScript")
+    incomeInput.value = '';
+    // input pocket money   
+
+    const pocketMoneyInput = document.getElementById("pocketMoney-input-box");
+    const pocketMoneyValue = pocketMoneyInput.value ;
+    const pocketMoneyText = parseInt(pocketMoneyValue);
+    // input home rent   
+    
+    const homeRentInput = document.getElementById("homeRent-input-box");
+    const homeRentValue = homeRentInput.value ;
+    const homeRentText = parseInt(homeRentValue);
+    // input food cost   
+    
+    const foodMoneyInput = document.getElementById("food-input-box");
+    const foodMoneyValue = foodMoneyInput.value ;
+    const foodMoneyText = parseInt(foodMoneyValue);
+
+    // divided by spending total expences
+    const totalExpenses = document.getElementById("display-total-expense");
+    const totalExpensesText = totalExpenses.innerText;
+    totalExpenses.innerText = pocketMoneyText + homeRentText + foodMoneyText;
+
+ 
+    // remained balance after spending 
+    const balance = document.getElementById("display-balance");
+    const balanceText = balance.innerText;
+    balance.innerText = incomeText - totalExpensesText;
+
+ 
+
 })
